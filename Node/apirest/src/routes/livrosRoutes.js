404f5e7098e8a322d824eede58187ae8225185d1
wmvj <warleyjunior05@gmail.com>
2023-01-31 14:54:39 -0300

    Alteração relacionado ao express e mongodb

---
+++ Node/apirest/src/routes/livrosRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import livroController from "../controllers/livrosController.js";
+
+const router = express.Router();
+
+router
+    .get("/livros", livroController.listarLivros)
+    .get("/livros/busca",livroController.listarLivroPorEditora)
+    .get("/livros/:id", livroController.listarLivroPorId)
+    .post("/livros", livroController.cadastrarLivro)
+    .put("/livros/:id", livroController.atualizarLivro)
+    .delete("/livros/:id", livroController.excluirLivro)
+    
+export default router;

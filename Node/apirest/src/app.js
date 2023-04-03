b8cad8d65fd91e1af53c6db55dcc185b3983a7f1
wmvj <warleyjunior05@gmail.com>
2023-04-03 13:16:56 -0300

    Alteração para a porta 7000 e uso do CORs

--- Node/apirest/src/app.js
+++ Node/apirest/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js"
 import routes from "./routes/index.js";
+import cors from 'cors'
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 db.once("open", () => {
@@ -8,6 +9,7 @@ db.once("open", () => {
 })
 
 const app = express();
+app.use(cors())
 app.use(express.json())
 routes(app)
 
